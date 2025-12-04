a3a1698da52dfedf3f96db331e975e59bce94ad6
Bachagha Mousaab <Mossbachagha@gmail.com>
2025-12-04 18:12:50 +0100

    full day1

--- src/main/java/com/proglobby/Main.java
+++ src/main/java/com/proglobby/Main.java
@@ -12,6 +12,7 @@ public class Main {
         String line;
         int target = 50;
         int count = 0;
+        /*
         while ((line = reader.readLine()) != null) {
             char direction = line.charAt(0);
             int value = Integer.parseInt(line.substring(1));
@@ -25,6 +26,54 @@ public class Main {
                 count ++;
             }
         }
+
+         */
+
+//        while ((line = reader.readLine()) != null) {
+//
+//            char direction = line.charAt(0);
+//            int oldTarget = target;
+//
+//            int value = Integer.parseInt(line.substring(1));
+//            if (direction == 'R') {
+//                target += value;
+//            } else{
+//                target -= value;
+//            }
+//            count +=((target) < 0 ? -((target / 100)-1) : ((target / 100)));
+//            count += target == 0 ? 1 : 0;
+//            if (oldTarget == 0 && (target < 0 || target >= 100)){
+//                count --;
+//            }
+//
+//            target = ((target % 100) + 100) % 100;
+//
+//        }
+
+        while ((line = reader.readLine()) != null){
+            char direction = line.charAt(0);
+            int value = Integer.parseInt(line.substring(1));
+            if (direction == 'R'){
+                if (target + value >= 100){
+                    count += (target+value)/100;
+                }
+                target += value;
+            }else{
+                if (target - value <= 0){
+
+                    count += (-(target - value)+100)/100;
+                    if (target == 0){
+                        count --;
+                    }
+                }
+                target -= value;
+            }
+
+
+            target = ((target % 100) + 100) % 100;
+            System.out.println(count  + " : " + target);
+        }
+
         System.out.println(count);
     }
 }
